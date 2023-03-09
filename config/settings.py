b74648020c09d18012810694ad3779ab441508aa
pyfbsdk59 <pyfbsdk59@gmail.com>
2023-03-09 15:17:43 +0800

    5c

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 DEBUG = True #可改為False
 
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
